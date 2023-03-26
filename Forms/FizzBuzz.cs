98faf69c3d483ffdc1961f85625e9b78a14efa17
Piotr2022 <piotr.mojkowski@wp.pl>
2023-03-26 21:52:57 +0200

    Wprawka 2

---
+++ Forms/FizzBuzz.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace FizzBuzzWeb.Forms
+{
+    public class FizzBuzz
+    {
+        [Display(Name = "Twój szczęśliwy numerek")]
+        [Required(ErrorMessage = "Pole jest obowiązkowe"), Range(1, 1000, ErrorMessage = "Oczekiwana wartość {0} z zakredu {1} i {2}.")]
+        public int? Number { set; get; }
+
+        public string CheckNumber(int? number)
+        {
+            string message = "";
+            if (number % 3 == 0)
+            {
+                message += "Fizz";
+            }
+            if (number % 5 == 0)
+            {
+                message += "Buzz";
+            }
+
+            if (message == "")
+            {
+                message = "Liczba: " + number + " nie spełnia kryteriów FizzBuzz";
+            }
+
+            return message;
+        }
+
+    }
+
+
+    
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using FizzBuzzWeb.Forms;
+
+namespace FizzBuzzWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public FizzBuzz FizzBuzz { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string? Name { get; set; }
+
+        public string? Message { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if(string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            Message = FizzBuzz.CheckNumber(FizzBuzz.Number);
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+            return Page();
+
+            /*if (ModelState.IsValid)
+            {
+                return Page();
+            }
+            return RedirectToPage("./Privacy");*/
+        }
+    }
+}
